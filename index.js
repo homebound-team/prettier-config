bdfdae2c2e99043a9d17d770e63c0653eb6a2ed8
Ben Limmer <630449+blimmer@users.noreply.github.com>
2025-12-11 16:54:39 -0700

    feat: initial @homebound/prettier-config setup (#1)

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-organize-imports"],
+  trailingComma: "all",
+  printWidth: 120,
+};
+
+module.exports = config;
